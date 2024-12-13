db321b3d7af30a8656233ac5822e9c684d79a596
Arye <aryemunthe26@gmail.com>
2024-12-14 00:08:18 +0700

    Initial commit for front-end setup
    struktur folder untuk FE

---
+++ front-end/src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
